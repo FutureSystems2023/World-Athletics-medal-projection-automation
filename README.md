5f21feaad65c41fdf7ad6eb7dfbd7991c7868611
King Yeung Lee <kingyeung129@hotmail.com>
2023-04-30 17:12:20 +0800

    Edits to accomodate for World Athletics data. Successful test run.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # **Documentation**
 
-This R script is to automate the processes of medal projection for FINA results collected after running <a href="https://github.com/FSonline23/FINA-data-collection">FINA-data-collection/script.py</a>. 2 CSVs will be outputed (1 for the results of Shapiro Wilk's normality tests, 1 for compiling the datasets to be used for simulation)
+This R script is to automate the processes of medal projection for World Athletics results collected after running <a href="https://github.com/FSonline23/World-Athletics-data-collection">World-Athletics-data-collection/script.py</a>. 2 CSVs will be outputed (1 for the results of Shapiro Wilk's normality tests, 1 for compiling the datasets to be used for simulation)
 
 <hr>
 
@@ -16,3 +16,8 @@ Change into current directory and run R install.packages() to install the requir
 Ensure that R is installed and added to environment path before proceeding. In your terminal or your preferred CLI, Change into directory of normality_test.R script and run the following command.
 
 <pre><code>Rscript normality_test.R</code></pre>
+<hr>
+
+## **What does the Script do?**
+
+The script retrieves the data from "ALL_COUNTRIES" worksheet in the target excel workbook. Results will be extracted based on the name of athletes (namelist.csv) and discipline arguments supplied.
--- normality_test.R
+++ normality_test.R
@@ -8,11 +8,13 @@ library(lubridate)
 
 conflicts_prefer(dplyr::filter)
 
-# Read Excel from filepath and assign to dataframe object variables
-filePath <- "F 200 Freestyle.xlsx"
+
+# Read Excel from filepath and assign to dataframe object variable
+filePath <- "finalFilteredCleanedResults.xlsx"
 athlete_data_2019to2023 <- read_excel(filePath, sheet = "Competitors 2019-2023")
 athlete_data_2022to2023 <- read_excel(filePath, sheet = "Competitors 2022-2023")
 athlete_data_2023 <- read_excel(filePath, sheet = "Competitors 2023")
+
 
 # Create 2 Empty DataFrames, 1 with columns (Shapiro Wilk test) and 1 for compiling the different datasets used
 columns = c("Name", "Shapiro-Wilks_p-value", "Dataset_Used", "Total_Datapoints", "Datapoints_Used", "Remarks")
@@ -20,13 +22,14 @@ df_shapiroTestResults = data.frame(matrix(nrow = 0, ncol = length(columns)))
 colnames(df_shapiroTestResults) = columns
 df_results = data.frame()
 
-# Filter Unqiue Athlete, Loop through each Athlete and perform Shapiro Wilk's Test
-UniqueAthlete <- unique(athlete_data_2019to2023$full_name_computed)
-for (i in 1:length(UniqueAthlete)) {
-  print(UniqueAthlete[i])
+
+# Filter Unqiue Athlete, Loop through each Athlete and perform Shapiro Wilk' s Test
+UniqueAthletes <- unique(athlete_data_2019to2023$athlete_name)
+for (i in 1:length(UniqueAthletes)) {
+  print(UniqueAthletes[i])
   dataset_used <- "2023"
   remarks <- ""
-  results <- filter(athlete_data_2023, full_name_computed == UniqueAthlete[i])
+  results <- filter(athlete_data_2023, athlete_name == UniqueAthletes[i])
   top50pctResultsRows <- ceiling(nrow(results) / 2)
   Athlete <- head(results, top50pctResultsRows)
 
@@ -34,7 +37,7 @@ for (i in 1:length(UniqueAthlete)) {
   if (nrow(Athlete) < 3) {
     dataset_used <- "2022-2023"
     remarks <- paste("Not enough data points for 2023 (total of ", nrow(Athlete), " datapts)")
-    results <- filter(athlete_data_2022to2023, full_name_computed == UniqueAthlete[i])
+    results <- filter(athlete_data_2022to2023, athlete_name == UniqueAthletes[i])
     top50pctResultsRows <- ceiling(nrow(results) / 2)
     Athlete <- head(results, top50pctResultsRows)
   }
@@ -42,7 +45,7 @@ for (i in 1:length(UniqueAthlete)) {
   if (nrow(Athlete) < 3) {
     dataset_used <- "2019-2023"
     remarks <- paste("Not enough data points for 2022-2023 (total of ", nrow(Athlete), " datapts)")
-    results <- filter(athlete_data_2019to2023, full_name_computed == UniqueAthlete[i])
+    results <- filter(athlete_data_2019to2023, athlete_name == UniqueAthletes[i])
     top50pctResultsRows <- ceiling(nrow(results) / 2)
     Athlete <- head(results, top50pctResultsRows)
   }
@@ -51,7 +54,7 @@ for (i in 1:length(UniqueAthlete)) {
     remarks <- paste("Not enough data points for 2019-2023 (total of ", nrow(Athlete), " datapts)")
 
     # Append Athlete normality result to dataframe
-    df_shapiroTestResults[nrow(df_shapiroTestResults) + 1,] = c(UniqueAthlete[i], "NA", dataset_used, nrow(results), top50pctResultsRows, remarks)
+    df_shapiroTestResults[nrow(df_shapiroTestResults) + 1,] = c(UniqueAthletes[i], "NA", dataset_used, nrow(results), top50pctResultsRows, remarks)
     # print(paste("Final Shapiro wilk p-value: NA. Dataset Used:", dataset_used))
 
     # Append Athlete dataset to dataframe
@@ -59,18 +62,17 @@ for (i in 1:length(UniqueAthlete)) {
     next
   }
 
-  Athlete$swim_time <- period_to_seconds(ms(Athlete$swim_time))
-  shap_test = shapiro.test(Athlete$swim_time)
+  shap_test = shapiro.test(Athlete$mark)
 
   # Conditional Checks for Shapiro Wilk Normality test of Dataframe Object. If less than 0.05, escalate to next dataset.
   if (shap_test$p.value < 0.05) {
     dataset_used <- "2022-2023"
     remarks <- paste("Shapiro Wilk not normal for 2023 (p-value = ", shap_test$p.value, ")")
     # print("Shapiro-wilk test lesser than 0.05, trying 2022 to 2023 dataset")
-    top50pctResultsRows <- round(nrow(Athlete) / 2, digits = 0)
-    Athlete <- head(filter(athlete_data_2022to2023, full_name_computed == UniqueAthlete[i]), top50pctResultsRows)
-    Athlete$swim_time <- period_to_seconds(ms(Athlete$swim_time))
-    shap_test = shapiro.test(Athlete$swim_time)
+    top50pctResultsRows <- ceiling(nrow(Athlete) / 2)
+    Athlete <- head(filter(athlete_data_2022to2023, athlete_name == UniqueAthletes[i]), top50pctResultsRows)
+    Athlete$mark <- period_to_seconds(ms(Athlete$mark))
+    shap_test = shapiro.test(Athlete$mark)
     # print(shap_test$p.value)
   }
 
@@ -78,10 +80,10 @@ for (i in 1:length(UniqueAthlete)) {
     dataset_used <- "2019-2023"
     remarks <- paste("Shapiro Wilk not normal for 2022-2023 (p-value = ", shap_test$p.value, ")")
     # print("Shapiro-wilk test lesser than 0.05, trying 2019 to 2023 dataset")
-    top50pctResultsRows <- round(nrow(Athlete) / 2, digits = 0)
-    Athlete <- head(filter(athlete_data_2019to2023, full_name_computed == UniqueAthlete[i]), top50pctResultsRows)
-    Athlete$swim_time <- period_to_seconds(ms(Athlete$swim_time))
-    shap_test = shapiro.test(Athlete$swim_time)
+    top50pctResultsRows <- ceiling(nrow(Athlete) / 2)
+    Athlete <- head(filter(athlete_data_2019to2023, athlete_name == UniqueAthletes[i]), top50pctResultsRows)
+    Athlete$mark <- period_to_seconds(ms(Athlete$mark))
+    shap_test = shapiro.test(Athlete$mark)
     # print(shap_test$p.value)
   }
 
@@ -89,15 +91,15 @@ for (i in 1:length(UniqueAthlete)) {
     remarks <- paste("Shapiro Wilk not normal for 2019-2023 (p-value = ", shap_test$p.value, ")")
   }
 
+
   # Append Athlete normality result to dataframe
-  df_shapiroTestResults[nrow(df_shapiroTestResults) + 1,] = c(UniqueAthlete[i], shap_test$p.value, dataset_used, nrow(results), top50pctResultsRows, remarks)
+  df_shapiroTestResults[nrow(df_shapiroTestResults) + 1,] = c(UniqueAthletes[i], shap_test$p.value, dataset_used, nrow(results), top50pctResultsRows, remarks)
   print(paste("Final Shapiro wilk p-value:", shap_test$p.value, ". Dataset Used:", dataset_used))
 
   # Append Athlete dataset to dataframe
   df_results = rbind(df_results, Athlete)
 }
 
-# print(df_shapiroTestResults)
-
+# Write out analysis to csvs (one for shaprio test results, one for the dataset used)
 write.csv(df_shapiroTestResults, "shapiroTest.csv", row.names = FALSE)
 write.csv(df_results, "dataset.csv", row.names = FALSE)
